353bca07c00889f734bec4a6a96141d10848a4e9
rahee-k-it <108662234+rahee-k-it@users.noreply.github.com>
2022-07-15 15:13:02 +0900

    카드 예시 문구 변경

--- src/pages/CardPage.js
+++ src/pages/CardPage.js
@@ -4,11 +4,7 @@ function CardPage() {
   return (
     <>
       <div>
-        <Card
-          bgImg="assets/image/cake.png"
-          title="당근 케이크"
-          content={<>맛있음. 우유랑 먹는거 추천</>}
-        />
+        <Card bgImg="assets/image/cake.png" title="Title" content="Content" />
       </div>
     </>
   );
