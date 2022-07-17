a006db5437546d4f09c00103e1d3cc07eba12c11
Chi Hwan Kim <rlaclghks123@naver.com>
2022-07-17 20:19:32 +0900

    hoverFooter 생성

--- src/pages/FooterPage.js
+++ src/pages/FooterPage.js
@@ -1,23 +1,33 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import HoverFooter from "../components/Footers/HoverFooter/HoverFooter";
+import PartitionFooter from "../components/Footers/PartitionFooter/PartitionFooter";
+import UnderLineFoooter from "../components/Footers/UnderLineFooter/UnderLineFooter";
 
 const Li = styled.li`
   width: 100%;
+  margin: 100px;
 `;
 function FooterPage() {
   return (
     <>
       <ol>
         <Li>
-          <Link to={"partition"}>Partition Footer</Link>
+          <Link to={"partition"}>
+            <PartitionFooter />
+          </Link>
         </Li>
 
         <Li>
-          <Link to={"underline"}>UnderLine Footer</Link>
+          <Link to={"underline"}>
+            <UnderLineFoooter />
+          </Link>
         </Li>
 
         <Li>
-          <Link to={"hover"}>Hover Footer</Link>
+          <Link to={"hover"}>
+            <HoverFooter />
+          </Link>
         </Li>
       </ol>
     </>
